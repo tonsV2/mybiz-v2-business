a38c06b4a3db1d88729efe37ed74bbce06978073
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-29 21:57:37 +0200

    Added oauth security.

--- src/main/java/dk/fitfit/mybiz/business/service/UserService.java
+++ src/main/java/dk/fitfit/mybiz/business/service/UserService.java
@@ -1,12 +1,55 @@
 package dk.fitfit.mybiz.business.service;
 
 import dk.fitfit.mybiz.business.domain.User;
+import dk.fitfit.mybiz.business.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
-public class UserService extends CrudService<User, Long> implements UserServiceInterface {
+public class UserService extends CrudService<User, Long> implements UserServiceInterface, UserDetailsService {
 	UserService(JpaRepository<User, Long> repository) {
 		super(repository);
 	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = ((UserRepository) repository).findByUsername(username);
+
+//					Collection<Role> roles = user.getRoles();
+//					Set<GrantedAuthority> authorities = roles
+//							.stream()
+//							.map(role -> (GrantedAuthority) role::getName)
+//							.collect(Collectors.toSet());
+
+		return new org.springframework.security.core.userdetails.User(user.getUsername(),
+				user.getPassword(),
+				true,
+				true,
+				true,
+				true,
+				Collections.EMPTY_LIST);
+	}
+
+	@Override
+	public User save(String username, String password, String email) {
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(passwordEncoder().encode(password));
+		user.setEmail(email);
+		return save(user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
--- src/main/java/dk/fitfit/mybiz/business/service/UserServiceInterface.java
+++ src/main/java/dk/fitfit/mybiz/business/service/UserServiceInterface.java
@@ -3,4 +3,5 @@ package dk.fitfit.mybiz.business.service;
 import dk.fitfit.mybiz.business.domain.User;
 
 public interface UserServiceInterface extends CrudServiceInterface<User, Long> {
+	User save(String username, String password, String email);
 }
